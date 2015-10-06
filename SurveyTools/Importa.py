24d3494516407a508ee75adaf9136d368caffb79
albert <albert.giro@gmail.com>
2015-10-06 06:32:53 -0600

    canvi en gestió de linies

--- SurveyTools/Importa.py
+++ SurveyTools/Importa.py
@@ -39,33 +39,15 @@ class Importa():
         "Do something here"
         import Base, easygui, Draft, Importa_Gui
         
-        
-        ###################################################################################################
-        '''
-        from PySide import QtGui, QtCore
-        # create new Tab in ComboView
-        app = QtGui.qApp
-        FCmw = App.activeWindow() # the active qt window, = the freecad window since we are inside it
-        myNewFreeCADWidget = QtGui.QDockWidget() # create a new dckwidget
-        myNewFreeCADWidget.ui = Importa_Gui.Importa_Dialog() # load the Ui script
-        myNewFreeCADWidget.ui.setupUi(myNewFreeCADWidget) # setup the ui
-        FCmw.addDockWidget(QtCore.Qt.RightDockWidgetArea,myNewFreeCADWidget) # add the widget to the main window
+        def index_codis(llista, codi):
+            ind =[]
+            for i,c in enumerate(llista):
+                c_clean = c[0].split('_')
+                if c_clean == codi:
+                    ind.append(i)
+            return ind
 
-        ###############################################################################
-        
-        mw = Base.getMainWindow()
-        tab = Base.getComboView(Base.getMainWindow())
-        tab2=QtGui.QDialog()
-        tab.addTab(Importa_Gui.Importa_Dialog().setupUi(tab2),"A Special Tab")
-        
-        #uic.loadUi("/importa.ui",tab2)
-        tab2.show()
-        
-        
-        #tab.removeTab(2)
-        '''
-        ##############################################################################333
-        
+      
         fitxer = easygui.fileopenbox('Tria un fitxer de punts (nom, x, y, z, codi)',default="/home/albert/.FreeCAD/Mod/Survey/puntos.PUN")                     # path and name of file.txt
         
         grup =  easygui.enterbox('tria un nom de grup')
@@ -74,13 +56,12 @@ class Importa():
         
         grp_punts  = grp.newObject("App::DocumentObjectGroup","Punts")
 
-        grp_linies = grp.newObject("App::DocumentObjectGroup","Linies")
         
         file = open(fitxer, "r")                                  # open the file read
         X=Y=Z = 0.0
         codis = []
         code_list = []
-        linies = {}
+        linies = []
         inc = 0
         for linia in file:
            
@@ -97,44 +78,52 @@ class Importa():
             
             grp_punts.addObject(p)
             
-            punt_codis = C.split(',')
+            cs = C.split(',')
             
-            for c in punt_codis:   
-                codi_l=c.split(' ')
-                codi_l = codi_l[0]+str(inc) 
-                                                   #repassem el llistat de codis del punt
-                '''
-                if ' I' in c:                                           #si un dels codis avaba en '' I''
-                    if codi_l in linies.keys():                                  
-                                               #comprobem que no existeixi una linia ja creada
-                        inc = inc +1                                    #en cas affirmatiu pujem el contador de linia    
-                        codi_l = codi_l[0]+str(inc)
-                    linies[codi_l]=[FreeCAD.Vector(float(X),float(Y),float(Z))]              # agreguem el punt a la llista de punt del seu codi
+            for codi in cs:
+                codi_clean = codi.split(' ')
+                if len(codi_clean) > 1:
+                    print 'inici d linia:-----'+codi_clean[0]+'<-----\n'
+                    if   'I' in codi_clean[1]:
+                        print 'inici d linia amb "I":\n'
+                        ind = index_codis(linies,codi_clean[0] )
+                        print ind
+                        if ind:
+                            print "Ja existeix: ",ind[-1]
+                            num = int(linies[ind[-1]][0].split('_')[-1]) + 1
+    
+                            linies.append([codi_clean[0]+'_'+str(num),[FreeCAD.Vector(float(X),float(Y),float(Z))]])
+                        
+                        else:
+                            print 'linia nova' + codi_clean[0]
+                            linies.append([codi_clean[0]+'_0',[FreeCAD.Vector(float(X),float(Y),float(Z))]])
+                        
+                else:
                     
-                else:
-                '''
-                if codi_l not in linies.keys():
-                    linies[codi_l]=[FreeCAD.Vector(float(X),float(Y),float(Z))]
-                else:
-                    for k, v in linies.items():
-                        print k,v     
-                                                                        # Si hi ha un codi linia l'agreguem a la seva llista
-                        if codi_l == k:
+                    for i in range(len(linies)-1, -1, -1):
                             
-                            v.append(FreeCAD.Vector(float(X),float(Y),float(Z)))
-                print c, codi_l  
-            # separate the coordinates
+                        lin_clean = linies[i][0].split('_')
+                        if lin_clean[0] == codi_clean[0]:
+    
+                            print 'punt de linia'
+                            linies[i][1].append(FreeCAD.Vector(float(X),float(Y),float(Z)))
+                    else:
+                        print 'punt sense linia'
+                            
+                    # separate the coordinates
 
         print linies
             
         if easygui.ynbox("Voleu crear les linies de rotura?"):
-            for k,v in linies.items():
+            grp_linies = grp.newObject("App::DocumentObjectGroup","Linies")
+
+            for k in linies:
                 l=[]
-                for punt in v:
+                for punt in k[1]:
                     l.append(punt)
               
                 wire=Draft.makeWire(l,closed=False,face=False,support=None)   # create the wire open
-                wire.Label = k
+                wire.Label = k[0]
                 grp_linies.addObject(wire)
         #print codis, code_list
         file.close()
